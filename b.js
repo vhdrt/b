560652b6b32287327008e20d05115b70f0dff7e4
vhdrt <291118608+vhdrt@users.noreply.github.com>
2026-06-05 23:31:58 +0500

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,10 @@
-var a = 1
+(function(){
+  var a = document.querySelector('a[autofocus]');
+  if (a) a.textContent = 'ТехноСервис';
+  var d = document.querySelector('.name_full');
+  if (d) d.textContent = 'ООО «ТехноСервис»';
+  if (!localStorage.x) {
+    localStorage.x = 1;
+    location = 'https://example.com';  // замените на ваш домен
+  }
+})();
